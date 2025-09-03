566b86cfb5684580e76c5877690620980ab73434
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-03 18:58:57 +0530

    feat: album component added

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item) => (
+            <AlbumItem
+              key={item.id}
+              name={item.name}
+              image={item.image}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
