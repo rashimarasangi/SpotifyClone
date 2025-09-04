c06aed9fa60849064db02dbc8429d31623b2ae2a
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-04 15:50:57 +0530

    feat: album and songItem added

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { albumsData } from "../assets/assets";
+import { albumsData, songsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
@@ -12,6 +13,20 @@ const DisplayHome = () => {
         <div className="flex overflow-auto">
           {albumsData.map((item) => (
             <AlbumItem
+              key={item.id}
+              name={item.name}
+              image={item.image}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's Big Hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item) => (
+            <SongItem
               key={item.id}
               name={item.name}
               image={item.image}
